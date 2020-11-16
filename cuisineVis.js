7933025ad8d16dff9eea03c1041ebfba41d00588
kgarrity22 <garrityk@bc.edu>
2020-11-15 22:28:15 -0500

    added svgs with margin conventions to each module

--- cuisineVis.js
+++ cuisineVis.js
@@ -1,6 +1,19 @@
 //Vis for comparing cuisines and how many of each type there are
 export default function CuisineVis(container) {
 
+    const margin = ({ top: 50, right: 50, bottom: 50, left: 50 })
+    const width = 1000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+
+    let svg = d3.select(container).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
 
 
 }
